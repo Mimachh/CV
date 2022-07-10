96e2606b78b8b0da82409d0bc9d2ee66138bcf5e
Mimachh <106066885+Mimachh@users.noreply.github.com>
2022-07-10 20:31:51 +0200

    Add files via upload

---
+++ CV2/script.js
@@ -0,0 +1,24 @@
+let competences = document.querySelector(".competences");
+let isShow =true;
+function showHide() {
+    if (isShow) {
+        competences.style.display = "none";
+        isShow = false;
+} else {
+    competences.style.display = "block";
+    isShow = true;
+} };
+
+
+let parcours = document.querySelector(".parcours");
+let isShow2 =true;
+function showHide2() {
+    if (isShow2) {
+        parcours.style.display = "none";
+        isShow2 = false;
+} else {
+    parcours.style.display = "block";
+    isShow2 = true;
+} };
+
+
